970413a81206fdccc22ff7d456cddb27fcc40645
fox@teamidea.ru <Zn2g47hZqeDvl>
2022-06-19 20:03:30 +0300

    Popup2 for images.

--- wp-content/themes/khl/js/popup.js
+++ wp-content/themes/khl/js/popup.js
@@ -23,9 +23,38 @@ $(document).on('click', '.js-popup-link', function (e) {
 	document.querySelector('html').classList.add('popup_active')
 })
 
+$(document).on('click', '.js-popup2-close', function (e) {
+	e.preventDefault()
+
+	document.querySelector('html').classList.remove('popup2_active')
+})
+
 // popup close button
 document.querySelectorAll('.js-popup-close').forEach((closeBtn) => {
 	closeBtn.addEventListener('click', () => {
 		document.querySelector('html').classList.remove('popup_active')
 	})
+})
+
+// popup2
+$('.js-popup2-show').click( function(e) {
+	e.preventDefault()
+
+	let popupContent    = document.querySelector('.popup2__content')
+	let popupImg        = '<img src="' + $(this).attr('href') + '" alt="" />'
+
+	document.querySelector('html').classList.add('popup2_active')
+
+	if (popupContent !== null) {
+		popupContent.innerHTML = popupImg
+
+		return
+	}
+
+	const popupContentNew = document.createElement('div')
+	popupContentNew.classList.add('popup2')
+
+	popupContentNew.innerHTML = '<div class="popup2__bg js-popup2-close"></div><div class="popup2__content"><div class="popup__close js-popup2-close"></div>' + popupImg +'</div>'
+
+	document.querySelector('html').append(popupContentNew)
 })
